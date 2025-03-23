0647e9163ec3ecfa1eb1035e3a51f1b9c1e5a7e9
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-23 15:52:42 +0100

    componsables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,20 @@
         >
           HOME VIEW
         </h1>
+        <div class="m-4 flex gap-2 justify-center">
+          <p class="border p-2 text-center">Mouse position(x): {{ x }}</p>
+          <p class="border p-2 text-center">Mouse position(y): {{ y }}</p>
+        </div>
       </div>
     </template>
   </MainLayout>
 </template>
 
 <script lang="ts" setup>
+import useMouse from "@/composables/useMouse";
 import MainLayout from "@/layouts/MainLayout.vue";
+
+const { x, y } = useMouse();
 </script>
 
 <style scoped></style>
